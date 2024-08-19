840de484b9d9f5ce9c9795d26591f85e0eef4832
joakimnagornoff <joakim.nagornoff@live.se>
2024-08-19 12:26:27 +0200

    feat: added applicationInformation and Request to models

---
+++ Nordax.Bank.Recruitment/ClientApp/src/models/loanApplicationInformation.ts
@@ -0,0 +1,6 @@
+export interface LoanApplicationInformation {
+  userId: string;
+  name: string;
+  email: string;
+  amount: string;
+}
---
+++ Nordax.Bank.Recruitment/ClientApp/src/models/newLoanApplicationRequest.ts
@@ -0,0 +1,5 @@
+export interface NewLoanApplicationRequest {
+  name: string;
+  email: string;
+  amount: number;
+}
